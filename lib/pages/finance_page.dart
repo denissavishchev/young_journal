ac63fc792292454514a5b0df126d61fa60c20b6f
Denis Savishchev <denis.savishchev@gmail.com>
2023-09-15 22:59:19 +0200

    add button

--- lib/pages/finance_page.dart
+++ lib/pages/finance_page.dart
@@ -20,41 +20,103 @@ class FinancePage extends StatelessWidget {
           return Column(
             children: [
               SizedBox(height: size.height * 0.07),
-              Center(
-                child: Container(
-                  width: size.width * 0.9,
-                  height: size.height * 0.08,
-                  decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(24)),
-                      gradient: LinearGradient(
-                          colors: [
-                            kOrange.withOpacity(0.3),
-                            Colors.transparent
-                          ],
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                          stops: const [0.1, 0.8]
-                      )
-                  ),
-                  child: Row(
-                    children: [
-                      const SizedBox(width: 40,),
-                      IconButton(
-                          onPressed: (){},
-                          icon: const Icon(Icons.arrow_back_ios, color: Colors.white,)),
-                      const SizedBox(width: 10,),
-                      const Text('Monday 23.03', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
-                      const SizedBox(width: 10,),
-                      IconButton(
-                          onPressed: (){},
-                          icon: const Icon(Icons.arrow_forward_ios, color: Colors.white,)),
-                      const Spacer(),
-                      BasicButtonWidget(
-                        onTap: () =>
-                            data.addTask(context),
-                        text: '+',),
-                    ],
-                  ),
+              Container(
+                margin: EdgeInsets.only(left: size.width * 0.05),
+                height: size.height * 0.08,
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(24)),
+                    gradient: LinearGradient(
+                        colors: [
+                          kOrange.withOpacity(0.3),
+                          Colors.transparent
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        stops: const [0.1, 0.8]
+                    )
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 40,),
+                    IconButton(
+                        onPressed: (){},
+                        icon: const Icon(Icons.arrow_back_ios, color: Colors.white,)),
+                    const SizedBox(width: 10,),
+                    const Text('Monday 23.03', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+                    const SizedBox(width: 10,),
+                    IconButton(
+                        onPressed: (){},
+                        icon: const Icon(Icons.arrow_forward_ios, color: Colors.white,)),
+                    const Spacer(),
+                    Container(
+                      width: 80,
+                      height: 65,
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.5),
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              bottomLeft: Radius.circular(30)),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                blurRadius: 3,
+                                offset: const Offset(0, 2)
+                            ),
+                          ]
+                      ),
+                      child: Center(
+                        child: Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.all(Radius.circular(30)),
+                              gradient: LinearGradient(
+                                  colors: [
+                                    const Color(0xffbebebc).withOpacity(0.5),
+                                    const Color(0xff1a1a18).withOpacity(0.8),
+                                  ],
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                  stops: const [0, 0.75]
+                              ),
+                            ),
+                            child: Center(
+                              child: GestureDetector(
+                                onTap: () => data.addTask(context),
+                                child: Container(
+                                  width: 48,
+                                  height: 48,
+                                  decoration: BoxDecoration(
+                                      color: const Color(0xff91918f),
+                                      border: Border.all(
+                                          color: kOrange,
+                                          width: 0.5),
+                                      borderRadius: const BorderRadius.all(Radius.circular(25)),
+                                      boxShadow: const [
+                                        BoxShadow(
+                                            color: Colors.black,
+                                            // spreadRadius: 2,
+                                            blurRadius: 3,
+                                            offset: Offset(0, 2)
+                                        ),
+                                        BoxShadow(
+                                            color: Color(0xff5e5e5c),
+                                            // spreadRadius: 2,
+                                            blurRadius: 1,
+                                            offset: Offset(0, -1)
+                                        ),
+                                      ]
+                                  ),
+                                  child: Icon(Icons.add,
+                                    color: kOrange.withOpacity(0.7),
+                                    size: 40,),
+                                ),
+                              ),
+                            )
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               SizedBox(height: size.height * 0.02,),
